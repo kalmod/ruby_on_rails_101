6d9fdfbabc40202f521a3d9dd43790ecb6118308
Kalmod <kalmod@live.com>
2020-02-20 14:41:59 -0500

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     def index
         # @ is an instance variable
         # A ruby instance variable is available to all methods in the instance
@@ -6,5 +7,40 @@ class ArticlesController < ApplicationController
     end
     def show
         @article = Article.find(params[:id])
+    end
+    def new
+        @article = Article.new
+    end
+    def create 
+        # @article = Article.new
+        # @article.title = params[:article][:title]
+        # @article.body = params[:article][:body]
+
+        # @article = Article.new(
+        #     title: params[:article][:title],
+        #     body: params[:article][:body]
+        # )
+
+        @article = Article.new(article_params)
+        @article.save
+        flash.notice = "Article '#{@article.title}' Created!"
+        redirect_to article_path(@article)
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash.notice = "Article '#{@article.title}' Deleted..."
+        redirect_to articles_path
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
     end
 end
